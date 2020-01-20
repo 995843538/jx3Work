2cf643c6c726d06e4760f837fda72039c2b3f89f
nanchen <995843538@qq.com>
2020-01-20 15:22:00 +0800

    test

---
+++ jx3work/src/main/java/com/jx3/Jx3MainApplication.java
@@ -0,0 +1,11 @@
+package com.jx3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jx3MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Jx3MainApplication.class);
+    }
+}
---
+++ jx3work/src/main/java/com/jx3/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.jx3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
